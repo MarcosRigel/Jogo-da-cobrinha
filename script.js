d5986215f8e61eaef2e8bf4f31ee61a1f0b2f8e6
Marcos Rigel <marcosrigel99@gmail.com>
2022-05-18 00:15:08 -0300

    terceira parte

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ snake[0] = {
   y: 8 * box
 }
 let direction = "right";
+let food = {
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
   context.fillStyle = "lightgreen";
@@ -15,15 +19,20 @@ function criarBG() {
 
 function criarCobrinha() {
   for(i=0; i<snake.length; i++) {
-    //context.fillStyle = "green";
+    context.fillStyle = "green";
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
+}
+
+function drawFood() {
+  context.fillStyle = "red";
+  context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
 
 function update(event) {
-  if(event.keyCoe == 37 && direction != "right") direction = "left";
+  if(event.keyCode == 37 && direction != "right") direction = "left";
   if(event.keyCode == 38 && direction != "down") direction = "up";
   if(event.keyCode == 39 && direction != "left") direction = "right";
   if(event.keyCode == 40 && direction != "up") direction = "down";
@@ -37,6 +46,7 @@ function iniciarJogo() {
 
   criarBG();
   criarCobrinha();
+  drawFood();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
